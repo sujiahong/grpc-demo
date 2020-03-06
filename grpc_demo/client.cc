4b1c383618601b0db81f46bbfdd94883d99bf7b7
sujiahong <1511877962@qq.com>
2020-03-06 21:15:16 +0800

    init

---
+++ grpc_demo/client.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#ifdef BAZEL_BUILD
+#include "examples/protos/hello.grpc.pb.h"
+#else
+#include "hello.grpc.pb.h"
+#endif
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using helloworld::HelloRequest;
+using helloworld::HelloReply;
+using helloworld::Greeter;
+
+class GreeterClient {
+public:
+	GreeterClient(std::shared_ptr<Channel> channel):stub_(Greeter::NewStub(channel)){}
+	std::string SayHello(const std::string& user) {
+		HelloRequest request;
+		request.set_name(user);
+
+		HelloReply reply;
+		ClientContext context;
+
+		Status status = stub_->SayHello(&context, request, &reply);
+
+		if (status.ok()) {
+			return reply.message();
+		}
+		else {
+			std::cout << status.error_code() << ":" << status.error_message() << std::endl;
+			return "RPC failded";
+		}
+	}
+private:
+	std::unique_ptr<Greeter::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+	std::string target_str;
+	std::string arg_str("--target");
+	if (argc > 1) {
+		std::string arg_val = argv[1];
+		size_t start_pos = arg_val.find(arg_str);
+		if (start_pos != std::string::npos) {
+			start_pos += arg_str.size();
+			if (arg_val[start_pos] == '=') {
+				target_str = arg_val.substr(start_pos + 1);
+			}
+			else {
+				std::cout << "The only correct argument syntax is --target=" << std::endl;
+				return 0;
+			}
+		}
+		else {
+			std::cout << "The only accepttable argument is --target=" << std::endl;
+			return 0;
+		}
+	}
+	else {
+		target_str = "localhost:50051";
+	}
+	GreeterClient greeter(grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+	std::string user("rrrr  ppcc");
+	std::string reply = greeter.SayHello(user);
+	std::cout << "Greeter received: " << reply << std::endl;
+	return 0;
+}
